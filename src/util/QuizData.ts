91fb950e377380ff012451762b4ee5785e443f65
Abdul Lateef <99482700+abdullateefsherani07@users.noreply.github.com>
2025-01-23 13:52:56 +0530

    Create QuizData.ts

---
+++ src/util/QuizData.ts
@@ -0,0 +1,10 @@
+export const quizData = {
+  question: "Who is Eleven's best friend?",
+  options: [
+    "Mike Wheeler",
+    "Dustin Henderson",
+    "Lucas Sinclair",
+    "Will Byers",
+  ],
+  correctAnswer: "Mike Wheeler",
+};
